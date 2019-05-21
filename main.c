ac9b6b620f8973ac18306b26cda8e453503cc246
Alberto Torrejón Valenzuela <ajedrecillo97@hotmail.es>
2019-05-21 14:06:51 +0200

    Corregida gol_get_cell junto nueva función gol_rule

--- main.c
+++ main.c
@@ -8,12 +8,14 @@
 void gol_init(int ini[X][Y]);
 void gol_print(int ini[X][Y]);
 void gol_step(int ini[X][Y],int fin[X][Y]);
+bool gol_rule(int ini[X][Y],int i,int j);
 int gol_count_neighbors(int ini[X][Y],int i,int j);
 bool gol_get_cell(int ini[X][Y],int i,int j);
 void gol_copy(int ini[X][Y],int fin[X][Y]);
 
 int main(){
 	int i = 0;
+
 	// Declara dos mundos
 	int ini[X][Y];
 	int fin[X][Y];
@@ -41,7 +43,6 @@ void gol_init(int ini[X][Y]){
 	ini[5][5]=1;
 	ini[5][6]=1;
 	ini[5][7]=1;
-
 }
 
 void gol_print(int ini[X][Y]){
@@ -56,33 +57,41 @@ void gol_print(int ini[X][Y]){
 }
 
 void gol_step(int ini[X][Y],int fin[X][Y]){
+	//Función para iterar
 	int	i,j;
-	for (i=1;i<X-1;i++){
-		for (j=1;j<Y-1;j++){
-		fin[i][j]=gol_get_cell(ini,i,j);
+	for (i=0; i<=X; i++){
+		for (j=1; j<=Y; j++){
+		fin[i][j]=gol_rule(ini,i,j);
 	}}
 	gol_copy(ini,fin);
+}
+
+bool gol_rule(int ini[X][Y],int i,int j){
+	switch (gol_count_neighbors(ini, i, j))
+	{
+	case 2: return ini[i][j];
+	case 3: return 1;
+	default: return 0;
+	}
 }
 
 int gol_count_neighbors(int ini[X][Y],int i,int j){
 	// Devuelve el número de vecinos
 	int contador = -ini[i][j];
-	for (int k = i-1; k <= i+1; k++)
-	{
-		for (int l = j-1; l <= j+1; l++)
-		{
+	for (int k = i-1; k <= i+1; k++){
+		for (int l = j-1; l <= j+1; l++){
 			contador += ini[k][l];
 		}	
 	}
 	return contador;
 }
 
-bool gol_get_cell(int ini[X][Y],int i,int j){
-	switch (gol_count_neighbors(ini, i, j))
-	{
-	case 2: return ini[i][j];
-	case 3: return 1;
-	default: return 0;
+bool gol_get_cell(int mundo[X][Y],int i,int j){
+	//Validación del mundo
+	if ( (i>X && i<0) || (j>Y && j<0) ){
+		return 0;
+	} else {
+		return mundo[i][j];
 	}
 }
 
